a1efb7e8e8d0eb31102de59ab98af8780e5e9801
Fibre44 <denis.nicolas@hotmail.fr>
2022-12-10 15:03:44 +0100

    feat : Mise en place de la récupération des informations de la déclaration + société + établissement (uniquement si 1 établissement)

--- lib/dsn.d.ts
+++ lib/dsn.d.ts
@@ -1,10 +1,46 @@
-type societyList = society[];
-type establishmentList = establishment[];
+type dsnObject = {
+    softwareName: string | undefined;
+    provider: string | undefined;
+    softwareVersion: string | undefined;
+    dsnVersion: string | undefined;
+    type: string | undefined;
+};
+type dsn = {
+    collection: string;
+    field: string;
+    dsnStructure: string;
+    value: string;
+};
+type societyObjet = {
+    siren: string | undefined;
+    nic: string | undefined;
+    apen: string | undefined;
+    adress1: string | undefined;
+    adress2: string | undefined;
+    adress3: string | undefined;
+    zipCode: string | undefined;
+    city: string | undefined;
+};
 type society = {
     collection: string;
     field: string;
     dsnStructure: string;
     value: string;
+};
+type establishmentObjet = {
+    siren: string | undefined;
+    nic: string | undefined;
+    apet: string | undefined;
+    adress1: string | undefined;
+    adress2: string | undefined;
+    adress3: string | undefined;
+    zipCode: string | undefined;
+    country: string | undefined;
+    idcc: string | undefined;
+    legalStatus: string | undefined;
+    opco: string | undefined;
+    city: string | undefined;
+    idEstablishment: number | undefined;
 };
 type establishment = {
     collection: string;
@@ -12,15 +48,19 @@ type establishment = {
     dsnStructure: string;
     value: string;
     siren: string;
+    idEstablishment: number;
 };
 export declare class DsnParser {
     private societyList;
     private establishmentList;
+    private dsnList;
     private extractions;
     init(dir: string): Promise<void>;
     addSociety(row: society): void;
     addEstablishment(row: establishment): void;
-    get society(): societyList;
-    get establishment(): establishmentList;
+    addDsn(row: dsn): void;
+    get dsn(): dsnObject;
+    get society(): societyObjet;
+    get establishment(): establishmentObjet;
 }
 export {};
--- src/dsn.ts
+++ src/dsn.ts
@@ -1,8 +1,36 @@
+import { throws } from 'node:assert';
 import fs from 'node:fs';
 import readline from 'node:readline';
 
 type societyList = society[]
 type establishmentList = establishment[]
+type dsnList = dsn[]
+
+type dsnObject = {
+    softwareName: string | undefined,
+    provider: string | undefined,
+    softwareVersion: string | undefined,
+    dsnVersion: string | undefined,
+    type: string | undefined,
+
+}
+type dsn = {
+    collection: string,
+    field: string,
+    dsnStructure: string,
+    value: string
+}
+
+type societyObjet = {
+    siren: string | undefined,
+    nic: string | undefined,
+    apen: string | undefined,
+    adress1: string | undefined,
+    adress2: string | undefined,
+    adress3: string | undefined,
+    zipCode: string | undefined,
+    city: string | undefined
+}
 
 type society = {
     collection: string,
@@ -10,12 +38,29 @@ type society = {
     dsnStructure: string,
     value: string
 }
+
+type establishmentObjet = {
+    siren: string | undefined,
+    nic: string | undefined,
+    apet: string | undefined,
+    adress1: string | undefined,
+    adress2: string | undefined,
+    adress3: string | undefined,
+    zipCode: string | undefined,
+    country: string | undefined,
+    idcc: string | undefined,
+    legalStatus: string | undefined,
+    opco: string | undefined,
+    city: string | undefined,
+    idEstablishment: number | undefined
+}
 type establishment = {
     collection: string,
     field: string,
     dsnStructure: string,
     value: string,
-    siren: string
+    siren: string,
+    idEstablishment: number
 }
 type extractions = extraction[]
 
@@ -30,10 +75,26 @@ type extraction = {
 export class DsnParser {
     private societyList: societyList = []
     private establishmentList: establishmentList = []
+    private dsnList: dsnList = []
     private extractions: extractions = [
         {
             collection: 'Dsn',
-            field: 'version',
+            field: 'softwareName',
+            dsnStructure: 'S20.G00.05.001',
+        },
+        {
+            collection: 'Dsn',
+            field: 'provider',
+            dsnStructure: 'S20.G00.05.002',
+        },
+        {
+            collection: 'Dsn',
+            field: 'softwareVersion',
+            dsnStructure: 'S20.G00.05.003',
+        },
+        {
+            collection: 'Dsn',
+            field: 'dsnVersion',
             dsnStructure: 'S10.G00.00.006',
         },
         {
@@ -80,11 +141,6 @@ export class DsnParser {
             collection: 'Society',
             field: 'city',
             dsnStructure: 'S21.G00.06.006',
-        },
-        {
-            collection: 'Society',
-            field: 'idcc',
-            dsnStructure: 'S21.G00.06.015',
         },
         {
             collection: 'Society',
@@ -156,11 +212,22 @@ export class DsnParser {
         for await (const line of rl) {
             let siren: string | null = null
             let lineSplit = line.split(',\'');
-            let dsnStructure = lineSplit[1]
+            let dsnStructure = lineSplit[0]
             let findStructure = this.extractions.find(d => d.dsnStructure === dsnStructure)
+            let idEstablishment = 0
             if (findStructure) {
                 let value = lineSplit[1].replace('\'', '')
                 switch (findStructure.collection) {
+                    case 'Dsn':
+                        if (lineSplit[0] === 'S21.G00.06.001') {
+                            siren = value
+                        }
+                        let addDsn: dsn = {
+                            ...findStructure,
+                            value: value
+                        }
+                        this.addDsn(addDsn)
+                        break
                     case 'Society':
                         if (lineSplit[0] === 'S21.G00.06.001') {
                             siren = value
@@ -172,10 +239,14 @@ export class DsnParser {
                         this.addSociety(addRowSociety)
                         break
                     case 'Establishment':
+                        if (line[0] === 'S.21.GOO.11.0001') {
+                            idEstablishment += 1
+                        }
                         let addRowEstablishment: establishment = {
                             ...findStructure,
                             value: value,
-                            siren: siren ? siren : ''
+                            siren: siren ? siren : '',
+                            idEstablishment
                         }
                         this.addEstablishment(addRowEstablishment)
                         break
@@ -201,16 +272,57 @@ export class DsnParser {
         }
         return
     }
-
-    get society(): societyList {
-        return this.societyList
+    addDsn(row: dsn): void {
+        const findRow = this.dsnList.find(r => r?.collection === row.collection && r.field === row.field)
+        if (!findRow) {
+            this.dsnList.push(row)
+        }
     }
 
-    get establishment(): establishmentList {
-        return this.establishmentList
+    get dsn(): dsnObject {
+        const dsnObject: dsnObject = {
+            softwareName: this.dsnList.find(d => d.dsnStructure === 'S20.G00.05.001')?.value,
+            provider: this.dsnList.find(d => d.dsnStructure === 'S20.G00.05.002')?.value,
+            softwareVersion: this.dsnList.find(d => d.dsnStructure === 'S20.G00.05.003')?.value,
+            dsnVersion: this.dsnList.find(d => d.dsnStructure === 'S20.G00.05.006')?.value,
+            type: this.dsnList.find(d => d.dsnStructure === 'S20.G00.05.008')?.value,
+        }
+        return dsnObject
     }
 
+    get society(): societyObjet {
+        const societyObjet: societyObjet = {
+            siren: this.societyList.find(s => s.dsnStructure === 'S21.G00.06.001')?.value,
+            nic: this.societyList.find(s => s.dsnStructure === 'S21.G00.06.002')?.value,
+            apen: this.societyList.find(s => s.dsnStructure === 'S21.G00.06.003')?.value,
+            adress1: this.societyList.find(s => s.dsnStructure === 'S21.G00.06.004')?.value,
+            zipCode: this.societyList.find(s => s.dsnStructure === 'S21.G00.06.005')?.value,
+            city: this.societyList.find(s => s.dsnStructure === 'S21.G00.06.006')?.value,
+            adress2: this.societyList.find(s => s.dsnStructure === 'S21.G00.06.007')?.value,
+            adress3: this.societyList.find(s => s.dsnStructure === 'S21.G00.06.008')?.value,
+        }
+        return societyObjet
+    }
 
+    get establishment(): establishmentObjet {
+        //Attention dans une DSN on peut avoir X établissements
+        const establishmentObjet: establishmentObjet = {
+            siren: this.establishmentList.find(e => e.dsnStructure === 'S21.G00.11.001')?.siren,
+            nic: this.establishmentList.find(e => e.dsnStructure === 'S21.G00.11.001')?.value,
+            apet: this.establishmentList.find(e => e.dsnStructure === 'S21.G00.11.002')?.value,
+            adress1: this.establishmentList.find(e => e.dsnStructure === 'S21.G00.11.003')?.value,
+            zipCode: this.establishmentList.find(e => e.dsnStructure === 'S21.G00.11.004')?.value,
+            city: this.establishmentList.find(e => e.dsnStructure === 'S21.G00.11.005')?.value,
+            adress2: this.establishmentList.find(e => e.dsnStructure === 'S21.G00.11.006')?.value,
+            adress3: this.establishmentList.find(e => e.dsnStructure === 'S21.G00.11.007')?.value,
+            country: this.establishmentList.find(e => e.dsnStructure === 'S21.G00.11.015')?.value,
+            legalStatus: this.establishmentList.find(e => e.dsnStructure === 'S21.G00.11.015')?.value,
+            idcc: this.establishmentList.find(e => e.dsnStructure === 'S21.G00.11.022')?.value,
+            opco: this.establishmentList.find(e => e.dsnStructure === 'S21.G00.11.023')?.value,
+            idEstablishment: this.establishmentList.find(e => e.dsnStructure === 'S21.G00.11.001')?.idEstablishment
+        }
+        return establishmentObjet
+    }
 
 }
 
